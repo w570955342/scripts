db5608f3b10ba560199a94b00b459321f4b9973d
wubangbang <hdyzwubangbang@163.com>
2024-06-13 20:04:29 +0800

    fix: 增加常用influx命令

--- influxdb/常用.sh
+++ influxdb/常用.sh
@@ -10,6 +10,44 @@ show databases
 show measurements
 drop database tsdb
 
+# docker复制文件
+# 从容器复制文件到宿主机
+docker cp my_container:/path/to/file /host/path
+
+# 从宿主机复制文件到容器
+docker cp /host/path/file my_container:/path/to
+
+# 从容器复制目录到宿主机
+docker cp my_container:/path/to/directory /host/path
+
+# 从宿主机复制目录到容器
+docker cp /host/path/directory my_container:/path/to
+
+# scp传输文件
+
+# 从本地复制到远程
+# 将本地主机上的文件复制到远程主机：
+scp local_file.txt user@remote_host:/remote/directory
+
+# 将本地主机上的目录递归复制到远程主机：
+scp -r /local/directory user@remote_host:/remote/directory
+
+# 从远程复制到本地
+# 将远程主机上的文件复制到本地主机：
+scp user@remote_host:/remote/file.txt /local/directory
+
+# 将远程主机上的目录递归复制到本地主机：
+scp -r user@remote_host:/remote/directory /local/directory
+
+# 在两台远程主机之间复制
+scp user1@remote_host1:/remote/file.txt user2@remote_host2:/remote/directory
+
+# 注意事项
+# 在使用 scp 命令传输文件时，如果没有使用密钥对进行身份验证，
+# 你将需要使用密码进行身份验证。通常情况下，
+# scp 命令会提示你输入密码，并且密码输入时不会回显。
+
+
 # 假设数据库有tsdb和tmp两个数据库
 # 备份可以指定数据库，或者全部
 # 全部
